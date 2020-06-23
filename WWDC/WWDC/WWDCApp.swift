9ce4997805ce972b7437adef68126221dd363eb5
Mitchell Budge <mitchellbudge@gmail.com>
2020-06-23 12:40:21 -0600

    Add base Xcode project

---
+++ WWDC/WWDC/WWDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCApp.swift
+//  WWDC
+//
+//  Created by Mitchell Budge on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct WWDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
